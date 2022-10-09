8f1fb9a4935054052f62b1f987db48b49d527d34
Moubs13 <matthew.moubray@gmail.com>
2022-10-09 17:01:48 -0500

    Added function/module to allow user to search database

---
+++ DiabetesFoodJournal/Services.EfCore/JournalService.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.EfCore
+{
+    public class JournalService : IJournalService
+    {
+        public Task<ICollection<Journalentry>> SearchEntries(string searchValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DiabetesFoodJournal/WebApi/Features/JournalSearch/JournalSearchEndpoint.cs
+++ DiabetesFoodJournal/WebApi/Features/JournalSearch/JournalSearchEndpoint.cs
@@ -1,15 +1,41 @@
-﻿namespace WebApi.Features.JournalSearch
+﻿using Services;
+using Services.EfCore;
+
+namespace WebApi.Features.JournalSearch
 {
     public class JournalSearchEndpoint : Endpoint<JournalSearchRequest, List<JournalSearchResponse>>
     {
+        public JournalSearchEndpoint(IJournalService journalService)
+        {
+            this.JournalService = journalService;
+        }
+
+        public IJournalService JournalService { get; }
+
         public override void Configure()
         {
             this.Get("api/journalEntries");
             this.AllowAnonymous();
         }
 
-        public override Task<List<JournalSearchResponse>> ExecuteAsync(JournalSearchRequest req, CancellationToken ct)
+        public override async Task<List<JournalSearchResponse>> ExecuteAsync(JournalSearchRequest req, CancellationToken ct)
         {
+            List<JournalSearchResponse> retVal = new List<JournalSearchResponse>();
+            var journalSearchEntries = await this.JournalService.SearchEntries(req.SearchValue).ConfigureAwait(false);
+
+            // Loop through journal search entries and create a list of journal serach response. 
+            foreach (var journalSearchEntry in journalSearchEntries)
+            {
+                retVal.Add(new JournalSearchResponse
+                {
+                    Id = journalSearchEntry.Id,
+                    Name = journalSearchEntry.Title,
+
+
+                })
+                    
+            }
+
             return Task.FromResult(new List<JournalSearchResponse>
             {
                 new JournalSearchResponse
